3fae0b00a5fc764e425ac17cafafeabe8a152038
ValeriiaZhubrieva <perchik010102@gmail.com>
2020-08-11 22:22:35 +0300

    added blog-page and update price-page

--- app/js/main.js
+++ app/js/main.js
@@ -39,6 +39,15 @@ $(function(){
         $('.btn-list').removeClass('active')
     });
 
+    $('.posts__tabs .tab, settings__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.posts__tabs, .settings__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.posts__tabs .tabs, .settings__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
+
 
 
 
